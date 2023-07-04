d2fa140beaa346cb1624aa522f452c47b14040e6
Alyoshka <alyoshkamyshko@gmail.com>
2023-07-04 08:54:51 -0700

    Add nav links to hope and shop pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import HomePage from './components/home-page/HomePage';
 import ShopPage from './components/shop-page/ShopPage';
 
@@ -7,6 +7,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+
+        <ul>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/shop'>Shop</NavLink></li>
+        </ul>
+
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
