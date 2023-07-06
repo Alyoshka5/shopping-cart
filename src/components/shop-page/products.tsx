849384139169b65338e619e507389a030cc70f9d
Alyoshka <alyoshkamyshko@gmail.com>
2023-07-06 09:00:35 -0700

    Add product objects to display on shop page

---
+++ src/components/shop-page/products.tsx
@@ -0,0 +1,82 @@
+const products = [
+    {
+      name: "Logitech MX Mechanical Keyboard",
+      price: 29.99,
+      rating: 4,
+      company: "Logitech",
+      description: [
+        "Mechanical keyboard with customizable RGB lighting",
+        "Durable and responsive keys for comfortable typing",
+        "Wired connection for reliable performance",
+        "Compatible with Windows and macOS",
+      ],
+      imageUrl: "https://m.media-amazon.com/images/I/61ge3d+MQwL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+      name: "MacBook Air 13\"",
+      price: 999.99,
+      rating: 5,
+      company: "Apple",
+      description: [
+        "Thin and lightweight design with high-resolution Retina display",
+        "Powerful processor and ample storage for seamless performance",
+        "Long battery life for all-day usage",
+        "Advanced security features and intuitive operating system",
+      ],
+      imageUrl: "https://m.media-amazon.com/images/I/51dafnlz6wL.jpg"
+    },
+    {
+      name: "Lumiman LED Lightbulb",
+      price: 12.99,
+      rating: 3,
+      company: "Lumiman",
+      description: [
+        "Smart LED lightbulb with customizable RGB colors",
+        "Compatible with voice assistants for easy control",
+        "Adjustable brightness and scheduling options",
+        "Energy-efficient and long-lasting",
+      ],
+      imageUrl: "https://m.media-amazon.com/images/I/510rNVKkZNL.jpg"
+    },
+    {
+      name: "Amazon Alexa - Echo Dot",
+      price: 49.99,
+      rating: 4,
+      company: "Amazon",
+      description: [
+        "Smart voice assistant with built-in speakers",
+        "Plays music, answers questions, and controls smart home devices",
+        "Voice recognition for personalized responses",
+        "Supports various entertainment and productivity apps",
+      ],
+      imageUrl: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6287/6287974_sd.jpg"
+    },
+    {
+      name: "XPRIT 6.5\" Hoverboard",
+      price: 199.99,
+      rating: 4,
+      company: "Segway",
+      description: [
+        "Self-balancing electric scooter with hoverboard design",
+        "Intuitive controls and smooth maneuverability",
+        "Long battery life and fast charging",
+        "Built-in Bluetooth speakers for music streaming",
+      ],
+      imageUrl: "https://m.media-amazon.com/images/I/61tC4ZrTiDL._AC_UF894,1000_QL80_.jpg"
+    },
+    {
+      name: "Dell S2421HS 24\" Full HD Monitor",
+      price: 249.99,
+      rating: 4,
+      company: "Dell",
+      description: [
+        "High-resolution display with vibrant colors",
+        "Wide viewing angles for immersive visual experience",
+        "Multiple connectivity options for versatile usage",
+        "Ergonomic design for comfortable viewing",
+      ],
+      imageUrl: "https://m.media-amazon.com/images/I/81vf4znYbaL.jpg"
+    },
+];
+
+export default products;
