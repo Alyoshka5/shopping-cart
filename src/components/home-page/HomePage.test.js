9556b4c0362afa19692d06afbc05efba53e771af
Alyoshka <alyoshkamyshko@gmail.com>
2023-07-06 20:12:58 -0700

    Remove useless semicolon

--- src/components/home-page/HomePage.test.js
+++ src/components/home-page/HomePage.test.js
@@ -1,7 +1,7 @@
 import rendrer from 'react-test-renderer';
 import HomePage from './HomePage'
 import { BrowserRouter } from 'react-router-dom';
-;
+
 
 it('renders home page content', () => {
     const pageTree = rendrer.create(<BrowserRouter><HomePage /></BrowserRouter>).toJSON();
