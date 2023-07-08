91844bbd77b8a769b09337b63f7494390dffc714
Alyoshka <alyoshkamyshko@gmail.com>
2023-07-07 21:18:58 -0700

    Add cart icon to nav bar on shop path

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,8 @@ import { NavLink, useLocation } from 'react-router-dom';
 import '../styles/NavBar.css';
 import Icon from '@mdi/react';
 import { mdiChip } from '@mdi/js';
+import { Routes, Route } from 'react-router-dom';
+import CartIcon from './CartIcon';
 
 export default function NavBar() {
     const currentPath = useLocation().pathname;
@@ -18,6 +20,10 @@ export default function NavBar() {
                 <li><NavLink to='/' className={`${currentPathClassName}-link`}>Home</NavLink></li>
                 <li><NavLink to='/shop' className={`${currentPathClassName}-link`}>Shop</NavLink></li>
             </ul>
+
+            <Routes>
+                <Route path='/shop' element={<CartIcon />} />
+            </Routes>
         </nav>
     )
 }
